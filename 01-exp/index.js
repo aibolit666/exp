85d85daa60918388cb10967d741faf7194bdef79
Pavel <pavel.priladyshev@gmail.com>
2023-01-26 18:45:03 +0300

    feat: add id res

--- 01-exp/index.js
+++ 01-exp/index.js
@@ -1,9 +1,35 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.end('hello world')
+  res.set('Content-Type', 'application/json')
+  fs.createReadStream(path.join(__dirname, './data/cities.json')).pipe(res)
+})
+
+app.get('/:id', (req, res) => {
+  fs.readFile(path.join(__dirname, './data/cities.json'), 'utf-8', (err, cities) => {
+
+    if (err) {
+      res.status(500)
+      res.end('error')
+      return
+    }
+
+    const citiesJson = JSON.parse(cities)
+
+    const city = citiesJson.find(city => city.id === req.params.id)
+
+    if (city) {
+      res.json(city)
+    } else {
+      res.status(404)
+      res.end('end')
+    }
+  })
+
 })
 
 app.listen(8080, () => console.log('listen on 8080'))
